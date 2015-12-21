46e16ad8873c0cdc35d7e6f8e55500c433b12de7
Caridy <cpatino@salesforce.com>
2015-12-21 17:48:56 -0500

    so more basic testing

---
+++ test/unit/module-status.js
@@ -0,0 +1,41 @@
+import expect from 'expect';
+import Loader from '../../src/loader';
+import Module from '../../src/module';
+import ModuleStatus from '../../src/module-status';
+
+describe('Reflect.Module.ModuleStatus', () => {
+    it('exports a class', () => {
+        expect(ModuleStatus).toBeA('function');
+    });
+    it('is subclasseable', () => {
+        class foo extends ModuleStatus {
+            constructor(loader, key, module) {
+                super(loader, key, module);
+            }
+        }
+        let o = new foo(new Loader(), 'some-key', new Module());
+        expect(o).toBeAn('object');
+        expect(o.load).toBeA('function');
+        expect(o.result).toBeA('function');
+        expect(o.resolve).toBeA('function');
+        expect(o.reject).toBeA('function');
+    });
+    it('1rd argument loader is required', () => {
+        expect(() => {
+            new ModuleStatus();
+        }).toThrow();
+    });
+    it('2rd argument key is required', () => {
+        expect(() => {
+            new ModuleStatus(new Loader());
+        }).toThrow();
+    });
+    it('3rd argument module is optional', () => {
+        let o = new ModuleStatus(new Loader(), 'some-key');
+        expect(o).toBeAn('object');
+        expect(o.load).toBeA('function');
+        expect(o.result).toBeA('function');
+        expect(o.resolve).toBeA('function');
+        expect(o.reject).toBeA('function');
+    });
+});
---
+++ test/unit/registry.js
@@ -0,0 +1,73 @@
+import expect from 'expect';
+import Loader from '../../src/loader';
+import Registry from '../../src/registry';
+
+describe('Registry', () => {
+
+    describe('new Loader().registry', () => {
+        let registry = new Loader().registry;
+
+        it('is an member of loader', () => {
+            expect(registry).toBeAn('object');
+        });
+        it('.constructor', () => {
+            expect(registry.constructor).toBeA('function');
+        });
+        it('@@iterator', () => {
+            expect(registry[Symbol.iterator]).toBeA('function');
+        });
+        it('.entries', () => {
+            expect(registry.entries).toBeA('function');
+        });
+        it('.keys', () => {
+            expect(registry.keys).toBeA('function');
+        });
+        it('.values', () => {
+            expect(registry.values).toBeA('function');
+        });
+        it('.get', () => {
+            expect(registry.get).toBeA('function');
+        });
+        it('.set', () => {
+            expect(registry.set).toBeA('function');
+        });
+        it('.has', () => {
+            expect(registry.has).toBeA('function');
+        });
+        it('.delete', () => {
+            expect(registry.delete).toBeA('function');
+        });
+        it('cannot be subclasseable', () => {
+            let prototype = Object.getPrototypeOf(registry);
+            expect(prototype).toBeAn('object');
+            function foo () {}
+            foo.prototype = Object.create(prototype);
+            foo.prototype.constructor = registry.constructor;
+            expect(() => {
+                new foo();
+            }).toThrow();
+        });
+    });
+
+    describe('new Registry()', () => {
+        let loader = new Loader();
+        let registry = loader.registry;
+
+        it('cannot be instantiate directly', () => {
+            expect(() => {
+                new Registry({});
+            }).toThrow();
+        });
+        it('cannot be subclasseable', () => {
+            let prototype = Object.getPrototypeOf(registry);
+            expect(prototype).toBeAn('object');
+            function foo () {}
+            foo.prototype = Object.create(prototype);
+            foo.prototype.constructor = loader.registry.constructor;
+            expect(() => {
+                new foo();
+            }).toThrow();
+        });
+    });
+
+});
